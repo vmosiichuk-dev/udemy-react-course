236d416d84f85f3f686a2f2cb8a8c8d49d597bed
vladnomad <vlad.nmd@gmail.com>
2023-09-01 18:42:24 +0200

    Recap: 124,125 – Components & StrictMode

--- react101/src/App.js
+++ react101/src/App.js
@@ -1,25 +1,69 @@
-import logo from "./logo.svg";
+import {Component, StrictMode} from "react";
 import "./App.css";
 
-function App() {
-return (
-<div className="App">
-	<header className="App-header">
-		<img src={logo} className="App-logo" alt="logo" />
-		<p>
-			Edit <code>src/App.js</code> and save to reload.
-		</p>
-		<a
-			className="App-link"
-			href="https://reactjs.org"
-			target="_blank"
-			rel="noopener noreferrer"
-		>
-			Learn React
-		</a>
-	</header>
-</div>
-);
+const Title = () => {
+	return <h2>Hello World!</h2>
 }
 
+const Input = () => {
+	const placeholder = "Type here";
+	const stylefield = {
+		width: "300px"
+	};
+	return <input 
+			placeholder={placeholder} 
+			style={stylefield} 
+			type="text" />
+}
+
+class Field extends Component {
+	render() {
+		const placeholder = "Type here";
+		const stylefield = {
+			width: "300px"
+		};
+		
+		return <input 
+				placeholder={placeholder} 
+				style={stylefield} 
+				type="text" />
+	}
+
+}
+
+function Button() {
+	/* const text = "Submit";
+	return <button>{text}</button> */
+	/* const submit = () => {
+		return "Submit";
+	}
+	return <button>{submit()}</button> */
+	/* return <button>{3+4}</button> */
+	/* const p = <p>Submit</p>
+	return <button>{p}</button> */
+	const text = "Submit";
+	const success = true;
+	/* return <button>{if (success) {
+		return "Success";
+	}}</button> */
+	/* if (success) {
+		text = "Success";
+	} */
+	return <button>{success ? "Success" : text}</button>
+}
+
+function App() {
+	return (
+		<div className="App">
+			<StrictMode>
+				<Title />
+			</StrictMode>
+			<Input />
+			<Field />
+			<Button />
+		</div>
+	);
+}
+
+export {Title};
 export default App;
--- react101/src/index.js
+++ react101/src/index.js
@@ -1,36 +1,15 @@
-import React from "react";
+import React, {StrictMode} from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import {Title} from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-/*
-const reactElement = React.createElement("h2", {className: "title"}, "Hello World!")
- 
-const reactElement = {
-	type: "h2",
-	props: {
-		className: "title",
-		children: "Hello World!"
-	}
-}
- */
-
-// {{}} Object Object
-
-const text = "Hello World!"
-const reactElement = (
-	<div>
-		<h2 className="title">Title: {text}</h2>
-		<input type="text" />
-		<button tabIndex={0}>Submit</button>
-	</div>
-)
 
 root.render(
-	reactElement,
-	<React.StrictMode>
-		<App />
-		{/*reactElement*/}
-	</React.StrictMode>
+	<StrictMode>
+		<App>
+			<Title/>
+		</App>
+	</StrictMode>
 );
